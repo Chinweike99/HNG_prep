b1c91c40a5625c422fa3ea6dfea8477c3a457d0f
Chinweike99 <innocentakwolu1@gmail.com>
2025-02-17 09:44:49 +0100

    feat: web ping

---
+++ website-uptime-monitor/notifier.py
@@ -0,0 +1,5 @@
+import datetime
+
+def notify(status, url, reason=""):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H: %M:%S")
+    print(f"[{timestamp} {url} is {status} {reason}]")
